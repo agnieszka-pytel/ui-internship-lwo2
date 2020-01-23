7bd353d681302f64f73cc62771aa0db44cca472d
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-23 14:11:06 +0100

    Task 5 completed with mock data

--- tasks/5_profile-lookup/profile-lookup.js
+++ tasks/5_profile-lookup/profile-lookup.js
@@ -1,5 +1,33 @@
 function lookupProfile(firstName, prop) {
-  // / NO DATA FOR CONTACTS
+  let names = CONTACTS.map((c) => c.firstName);
+  if (names.includes(firstName)) {
+    let foundContact = CONTACTS.find((c) => c.firstName === firstName);
+    let props = foundContact[prop];
+    return props === undefined ? 'No such property' : props;
+  } else {
+    return 'No such contact';
+  }
 }
+
+const CONTACTS = [
+  {
+    firstName: 'Kristian',
+    lastName: 'Vos',
+    likes: ['Soccer'],
+    number: 123,
+    address: 'Somewhere',
+  }, {
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    likes: ['Intriguing Cases', 'Violin'],
+    number: 234,
+    address: '221B Baker Street',
+  }, {
+    firstName: 'Harry',
+    likes: ['Quiddich'],
+  }, {
+    firstName: 'Akira',
+  },
+];
 
 export {lookupProfile};
