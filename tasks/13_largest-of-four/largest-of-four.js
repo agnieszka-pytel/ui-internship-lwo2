1409252a74bc11b33090ba55c230a163deea03bb
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-24 12:16:22 +0100

    Added function with reduce and additional test

--- tasks/13_largest-of-four/largest-of-four.js
+++ tasks/13_largest-of-four/largest-of-four.js
@@ -2,4 +2,9 @@ function largestOfFour(array) {
   return array.map((a)=> a.sort((x, y) => x-y).pop());
 }
 
-export {largestOfFour};
+function largestOfFourReduce(array) {
+  let callback = (max, current) => Math.max(max, current);
+  return array.map((a) => a.reduce(callback));
+}
+
+export {largestOfFour, largestOfFourReduce};
