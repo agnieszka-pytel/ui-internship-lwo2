fe9277ba53ecd2432f2ccdc3d8613a6f076211c0
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-28 12:34:59 +0100

    Add reverseString function from task 6

--- tasks/31_reverse-string-in-range/reverse-string-in-range.js
+++ tasks/31_reverse-string-in-range/reverse-string-in-range.js
@@ -1,6 +1,8 @@
+import {reverseString} from '../6_reverse-string/reverse-string';
+
 export function reverseStringInRange(str, range) {
   let toReplace = str.slice(range[0], range[1]+1);
   return str.replace(
       toReplace,
-      toReplace.split('').reverse().join(''));
+      reverseString(toReplace));
 }
