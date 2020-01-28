8a282a2807b48b3a19b94313175d3e3e1d3b7ee0
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-28 09:55:04 +0100

    Changed hardcoded length 3 to ending.length

--- tasks/17_truncate-string/truncate-string.js
+++ tasks/17_truncate-string/truncate-string.js
@@ -1,7 +1,7 @@
 export function truncateString(string, maxLen) {
   let ending = '...';
   if (string.length > maxLen) {
-    return (maxLen <= 3) ?
+    return (maxLen <= ending.length) ?
     string.slice(0, maxLen).concat(ending) :
     string.slice(0, maxLen-ending.length).concat(ending);
   }
