38d052a0d7d740ab3ed5e7d05570df44f5ddb26b
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-28 11:38:30 +0100

    Changed switch function

--- tasks/18_humanize-format/humanize-format.js
+++ tasks/18_humanize-format/humanize-format.js
@@ -1,5 +1,8 @@
 export function humanizeFormat(num) {
   let lastDigit = num%10;
+  if (!num) {
+    return '';
+  }
   switch (lastDigit) {
     case 1:
       return num+'st';
@@ -7,9 +10,7 @@ export function humanizeFormat(num) {
       return num+'nd';
     case 3:
       return num+'rd';
-    case 4: case 5: case 6: case 7: case 8: case 9: case 0:
-      return num+'th';
     default:
-      return '';
+      return num+'th';
   }
 }
