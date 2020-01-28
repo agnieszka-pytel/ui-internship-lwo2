d773c7da43e15bffb998e7d7e3e31883ae8baae0
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-28 15:29:23 +0100

    Changed function (task 21) to use more readable variables and .every method

--- tasks/21_check-letters/check-letters.js
+++ tasks/21_check-letters/check-letters.js
@@ -1,6 +1,6 @@
 export function checkLetters(arr) {
-  let includedLetters = arr[1].toLowerCase().split('')
-      .map((a) => arr[0].toLowerCase().indexOf(a))
-      .filter((a) => a !== -1);
-  return includedLetters.length === arr[1].length;
+  arr = arr.map((element) => element.toLowerCase());
+  let word = arr[0];
+  let letters = arr[1];
+  return letters.split('').every((letter) => word.includes(letter));
 }
