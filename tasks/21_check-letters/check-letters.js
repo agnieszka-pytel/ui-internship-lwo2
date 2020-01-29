86c56d7433c3f74ae2b064cf86043a3684085745
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-01-29 13:53:24 +0100

    Corrected function argument reassignment problem and assigning variables from array

--- tasks/21_check-letters/check-letters.js
+++ tasks/21_check-letters/check-letters.js
@@ -1,6 +1,5 @@
-export function checkLetters(arr) {
-  arr = arr.map((element) => element.toLowerCase());
-  let word = arr[0];
-  let letters = arr[1];
-  return letters.split('').every((letter) => word.includes(letter));
+export function checkLetters(array) {
+  let arrayLower = array.map(element => element.toLowerCase());
+  const [word, letters] = arrayLower;
+  return letters.split("").every(letter => word.includes(letter));
 }
