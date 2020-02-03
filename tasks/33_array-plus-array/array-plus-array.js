769e48aadb7a92ba8d778c5d3a78793088536e8d
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-03 16:50:51 +0100

    Task 33 - replaced forEach with map

--- tasks/33_array-plus-array/array-plus-array.js
+++ tasks/33_array-plus-array/array-plus-array.js
@@ -1,13 +1,7 @@
 export function arrayPlusArray(array1, array2) {
-  let shorterArray = (array1.length <= array2.length) ? array1 : array2;
-  let longerArray = (array1.length > array2.length) ? array1 : array2;
-  let sum = [];
-  longerArray.forEach((element, index) => {
-    if (shorterArray[index]) {
-      sum.push(element + shorterArray[index]);
-    } else {
-      sum.push(element + 0);
-    }
-  });
-  return sum;
+  let shorterArray = array1.length <= array2.length ? array1 : array2;
+  let longerArray = array1.length > array2.length ? array1 : array2;
+  return longerArray.map((element, index) =>
+    shorterArray[index] ? element + shorterArray[index] : element
+  );
 }
