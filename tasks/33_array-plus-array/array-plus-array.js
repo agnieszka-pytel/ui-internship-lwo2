e6571a82640cd9030f5e4c9977bf047d9dedda72
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-04 10:09:19 +0100

    Task 33 - Reduced variable assignment to one line

--- tasks/33_array-plus-array/array-plus-array.js
+++ tasks/33_array-plus-array/array-plus-array.js
@@ -1,6 +1,6 @@
 export function arrayPlusArray(array1, array2) {
-  let shorterArray = array1.length <= array2.length ? array1 : array2;
-  let longerArray = array1.length > array2.length ? array1 : array2;
+  let [shorterArray, longerArray] =
+    array1.length <= array2.length ? [array1, array2] : [array2, array1];
   return longerArray.map((element, index) =>
     shorterArray[index] ? element + shorterArray[index] : element
   );
