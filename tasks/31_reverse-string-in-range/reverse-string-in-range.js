4d28445123c2b14320f5ddd9b1fb8656e86f3ff0
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-04 10:13:11 +0100

    Task 31 - fixed formatting

--- tasks/31_reverse-string-in-range/reverse-string-in-range.js
+++ tasks/31_reverse-string-in-range/reverse-string-in-range.js
@@ -1,8 +1,6 @@
-import {reverseString} from '../6_reverse-string/reverse-string';
+import { reverseString } from "../6_reverse-string/reverse-string";
 
 export function reverseStringInRange(str, range) {
-  let toReplace = str.slice(range[0], range[1]+1);
-  return str.replace(
-      toReplace,
-      reverseString(toReplace));
+  let toReplace = str.slice(range[0], range[1] + 1);
+  return str.replace(toReplace, reverseString(toReplace));
 }
