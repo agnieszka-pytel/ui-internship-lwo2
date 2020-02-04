0311ed984c89c838a30e2ee36a36d3a767d199f4
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-04 14:13:14 +0100

    Task 40 - replace direct comparisons with function

--- tasks/40_find-friend/find-friend.js
+++ tasks/40_find-friend/find-friend.js
@@ -1,12 +1,14 @@
-const BLUE = "blue";
+function isBlue(friend) {
+  return friend === "blue";
+}
 
 export function findFriend(array) {
   return array.reduce((friends, element, index) => {
     if (element === "red") {
       if (
-        (array[index + 1] === BLUE && array[index + 2] === BLUE) ||
-        (array[index - 2] === BLUE && array[index - 1] === BLUE) ||
-        (array[index - 1] === BLUE && array[index + 1] === BLUE)
+        (isBlue(array[index + 1]) && isBlue(array[index + 2])) ||
+        (isBlue(array[index - 2]) && isBlue(array[index - 1])) ||
+        (isBlue(array[index - 1]) && isBlue(array[index + 1]))
       ) {
         friends += 1;
       }
