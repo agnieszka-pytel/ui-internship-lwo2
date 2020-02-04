1b71045145c80b68f227d1d63d795dec59693ce5
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-04 14:20:15 +0100

    Task 39 - moved long conditions to functions

--- tasks/39_unique-in-order/unique-in-order.js
+++ tasks/39_unique-in-order/unique-in-order.js
@@ -1,9 +1,17 @@
+function isLastIndex(index, elements) {
+  return index === elements.length - 1;
+}
+
+function isNextDifferent(index, elements, element) {
+  return !isLastIndex(index, elements) && elements[index + 1] !== element;
+}
+
 export function uniqueInOrder(sentence) {
   let elements = typeof sentence === "string" ? sentence.split("") : sentence;
   return elements.reduce((uniques, element, index) => {
     if (
-      index === elements.length - 1 ||
-      (index < elements.length - 1 && elements[index + 1] !== element)
+      isLastIndex(index, elements) ||
+      isNextDifferent(index, elements, element)
     ) {
       uniques.push(element);
     }
