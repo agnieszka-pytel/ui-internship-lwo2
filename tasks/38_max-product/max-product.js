5f188f33300dffd23ac848385dd2481782d655f3
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-04 14:22:38 +0100

    Task 38 - simplified reduce function

--- tasks/38_max-product/max-product.js
+++ tasks/38_max-product/max-product.js
@@ -1,10 +1,9 @@
 export function maxProduct(array) {
   return array
     .reduce((productArray, element, index) => {
-      if (index < array.length - 1) {
-        productArray.push(element * array[index + 1]);
-      }
-      return productArray;
+      return index < array.length - 1
+        ? productArray.concat(element * array[index + 1])
+        : productArray;
     }, [])
     .sort((a, b) => a - b)
     .pop();
