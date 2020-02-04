d549132c60db3ecbfa058c2b9bc0a5fca2962117
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-04 13:50:41 +0100

    Done task 43

---
+++ tasks/43_brackets-indexes/brackets-indexes.js
@@ -0,0 +1,19 @@
+export function findCloseIndex(sentence, beginIndex) {
+  let depth = 0;
+  let endingIndex = beginIndex;
+  for (let [index, element] of sentence
+    .slice(beginIndex)
+    .split("")
+    .entries()) {
+    if (element === "(") {
+      depth++;
+    } else if (element === ")") {
+      depth--;
+    }
+    if (depth === 0) {
+      endingIndex = index + beginIndex;
+      break;
+    }
+  }
+  return sentence[beginIndex] === ("(" || ")") ? endingIndex : -1;
+}
