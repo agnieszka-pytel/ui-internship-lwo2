3b4c2a8cd2eb7af834cc164010d6e202a6f16f05
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-02-06 13:29:41 +0100

    Add comment to clarify the solution and empty line to separate code blocks

--- tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
+++ tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
@@ -1,4 +1,9 @@
+/* This function is based on mathematical equation for sum of natural numbers from 1 to N
+which is SUM = n*(n+1)/2 
+*/
+
 export function sumAll(range) {
   let [start, end] = range[0] > range[1] ? range.reverse() : range;
+
   return (end * (end + 1) - (start - 1) * start) / 2;
 }
